e795d9d77f49e52e6442e3767a157263911d8d36
ASUS <qjwp52@durham.ac.uk>
2023-02-28 19:16:30 +0000

    home

--- SE - AI Skills Tool/ClientApp/src/app/app.module.ts
+++ SE - AI Skills Tool/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginComponent } from "./login/login.component";
 
 @NgModule({
@@ -16,8 +14,6 @@ import { LoginComponent } from "./login/login.component";
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     LoginComponent
   ],
   imports: [
@@ -26,8 +22,6 @@ import { LoginComponent } from "./login/login.component";
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'login', component: LoginComponent },
     ])
   ],

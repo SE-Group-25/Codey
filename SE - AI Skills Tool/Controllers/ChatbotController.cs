653f46d89606b415adb3e4227a9dd002f388a2c7
Nat Salt <nat@nat-salt.com>
2023-03-03 13:34:32 +0000

    created test controller ChatbotController

---
+++ SE - AI Skills Tool/Controllers/ChatbotController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using SE_AI_Skills_Tool.Services;
+
+
+namespace SE_AI_Skills_Tool.Controllers
+{
+    [ApiController]
+    // [Route("chatbot")]
+    [Route("[controller]")]
+    public class ChatbotController: ControllerBase
+    {
+        private string? sessionId;
+        private readonly IWatson _watson;
+
+        public ChatbotController(IWatson watson)
+        {
+            _watson = watson;
+        }
+            
+        [HttpGet("StartChat")]
+        public void StartChat()
+        {
+            _watson.CreateSession();
+        }
+
+        [HttpPost("Message")]
+        public string Message(string msgString)
+        {
+            if (sessionId == null)
+            {
+                sessionId = _watson.CreateSession();
+            }
+            return _watson.Message(msgString, sessionId);
+        }
+
+        // ToDo: Write function to close the chat session. 
+    }
+}

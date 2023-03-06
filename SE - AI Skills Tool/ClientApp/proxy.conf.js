3ade5d46f7ce2c8ab53b3fbbbc9a93193a1abc72
Nat Salt <nat@nat-salt.com>
2023-03-06 12:35:08 +0000

    updated angular CLI and registered endpoints for chatbot controller.

--- SE - AI Skills Tool/ClientApp/proxy.conf.js
+++ SE - AI Skills Tool/ClientApp/proxy.conf.js
@@ -6,7 +6,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      "/weatherforecast", "/chatbot/Message", "/chatbot/StartChat", "/chatbot/EndChat"
    ],
     target: target,
     secure: false,

0367487d5d529afcc085ddd18d2c2d849c12520d
Ben Shattock <ben.shattock@icloud.com>
2023-03-15 18:25:27 +0000

    Finally done-ish

--- SE - AI Skills Tool/ClientApp/src/app/courses/courses.component.ts
+++ SE - AI Skills Tool/ClientApp/src/app/courses/courses.component.ts
@@ -15,12 +15,49 @@ export class CoursesComponent {
   @Input() public profile: boolean = false;
   @Input() public courses: any = [];
 
+  public addedCourses: CourseResponseDto[] = [];
+  public coursesSearched: boolean = false;
+  private userDTo: UserDto = {};
+
+
   transferObject!: AddCourseToUserDto;
 
-  constructor(public authService: AuthService, private _chatbot: ChatbotService) { }
+  constructor(public authService: AuthService, private _chatbot: ChatbotService, private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+    if (this.authService.isAuthenticated && !this.coursesSearched) this.getUserCourses();
+  }
+
+  ngAfterViewChecked() {
+    if (!this.authService.isAuthenticated && this.coursesSearched) this.resetCourses();
+    if (this.authService.isAuthenticated && !this.coursesSearched) this.getUserCourses();
+  }
+
+  resetCourses() {
+    this.coursesSearched = false;
+    this.addedCourses = [];
+  }
+
+  getUserCourses() {
+    try {
+      this.userDTo.Id = this.authService.ID;
+      this.http.post<any>(this.baseUrl + 'User/GetUserCourses', this.userDTo!).subscribe({
+        next: (res: any) => {
+          this.addedCourses = res;
+        }
+      });
+      this.coursesSearched = true;
+    } catch (e: unknown) {
+      console.log("Courses not found.\n" + (e as Error).message);
+    }
+    console.log(this.addedCourses);
+  }
+
+  courseAdded(course: CourseResponseDto) {
+    return this.addedCourses.includes(course);
+  }
 
   addCourseToUser(course: CourseResponseDto) {
     // TODO: make call to add the course to user database.
+    this.addedCourses.push(course);
     this.transferObject = {course: course, userId: this.authService.ID};
     this._chatbot.addCourseToUser("User/AddCourseToUser", this.transferObject).subscribe({next: res => console.log(res)})
   }
